7e3ad74f9fe07937ce8dc1d1d0c933640b9c5843
Vadim Ushakov <igeekless@gmail.com>
2020-09-23 09:05:09 +0700

    Delete lxpanel config, add waterline config

---
+++ waterline/panels/panel2.js
@@ -0,0 +1,99 @@
+{
+  "global": {
+    "edge": "bottom",
+    "align": "center",
+    "edge_margin": 13,
+    "align_margin": 377,
+    "oriented_width_type": "request",
+    "oriented_width": 464,
+    "oriented_height": 38,
+    "output_target": "primary_monitor",
+    "custom_monitor": 0,
+    "round_corners": false,
+    "round_corners_radius": 7,
+    "rgba_transparency": true,
+    "stretch_background": false,
+    "background": false,
+    "background_file": "",
+    "transparent": true,
+    "alpha": 40,
+    "tint_color": "#000000",
+    "GtkWidgetName": "",
+    "visibility_mode": "gobelow",
+    "height_when_hidden": 1,
+    "set_strut": 0,
+    "use_font_color": false,
+    "font_color": "#000000",
+    "use_font_size": false,
+    "font_size": 10,
+    "icon_size": 32,
+    "padding_top": 0,
+    "padding_bottom": 0,
+    "padding_left": 0,
+    "padding_right": 0,
+    "applet_spacing": 3,
+    "background_mode": "color",
+    "background_color": "#ffffff"
+  },
+  "plugins": [
+    {
+      "settings": {},
+      "type": "menu",
+      "expand": false,
+      "padding": 0,
+      "border": 0,
+      "icon_size": 0
+    },
+    {
+      "settings": {
+        "path": "~",
+        "name": "",
+        "image": "",
+        "show_hidden": false,
+        "show_files": true,
+        "max_file_count": 10,
+        "show_file_size": false,
+        "show_icons": true,
+        "show_tooltips": true,
+        "sort_directories_by": "Name",
+        "sort_files_by": "Name",
+        "plain_view": false
+      },
+      "type": "dirmenu",
+      "icon_size": 0,
+      "expand": false,
+      "padding": 0,
+      "border": 0
+    },
+    {
+      "type": "pager",
+      "expand": false,
+      "padding": 0,
+      "border": 0,
+      "icon_size": 0
+    },
+    {
+      "settings": {
+        "speed": 1500,
+        "size": 40,
+        "cpuload_visible": true,
+        "cpuload_colors": "#0072B3,#0092E6,#00A3FF,#002F3D,#000000",
+        "memload_visible": true,
+        "memload_colors": "#00B35B,#00E675,#00FF82,#AAF5D0,#000000",
+        "netload_visible": false,
+        "netload_colors": "#FCE94F,#EDD400,#C4A000,#000000",
+        "swapload_visible": true,
+        "swapload_colors": "#8B00C3,#000000",
+        "loadavg_visible": false,
+        "loadavg_colors": "#D50000,#000000",
+        "diskload_visible": true,
+        "diskload_colors": "#C65000,#FF6700,#000000"
+      },
+      "type": "multiload",
+      "expand": false,
+      "padding": 0,
+      "border": 0,
+      "icon_size": 0
+    }
+  ]
+}
